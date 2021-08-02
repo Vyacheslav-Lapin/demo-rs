23e481a563d916d4c37970b529394bf2d64f6eea
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-08-02 15:08:30 +0300

    initial ++

--- src/main/java/com/luxoft/rs/resource/NoteResource.java
+++ src/main/java/com/luxoft/rs/resource/NoteResource.java
@@ -1,2 +1,18 @@
-package com.luxoft.rs.resource;public class NoteResource {
+package com.luxoft.rs.resource;
+
+//@Path("/users/{username}/notes")
+//@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class NoteResource {
+
+//  @Autowired
+//  private NoteRepository noteRepository;
+//
+//  @Autowired
+//  private UserRepository userRepository;
+//
+//  @GET
+//  public Collection<Note> getNotes(@PathParam("username") String username) {
+//    return noteRepository.findByUserUsername(username);
+//  }
 }
+
